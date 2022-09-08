e65f6d1bbb084bc371f33c6f6e0e51ab676afebb
Pablo Méndez <pablo-mendez09@hotmail.com>
2022-09-08 23:52:26 +0000

    Mi primer commit

--- src/js/component/ToDo.js
+++ src/js/component/ToDo.js
@@ -1,17 +1,21 @@
-import React, { useState } from 'react';
-
-const AnyComponent = () => {
-    const [todoValue, setTodo ] = useState('');
-}
+import React, {useState} from 'react';
 
 
 
 const ToDo = () => {
+	const [todoValue, setTodo ] = useState('');
+	const [todoList, setTodoList ] = useState([]);
+	const changeList = () => {
+		{setTodoList([...todoList, todoValue])}
+	}
 
 	return (
 		<div >
 			<h1>ToDos</h1>
-			<input type="text" placeholder="What needs to be done" onChange={e => setTodo(e.target.value)}/>
+			<input type="text" placeholder="What needs to be done" onChange={e => setTodo(e.target.value)} onClick={() => changeList()}/>
+			{todoList.map((item)=><li>{item}</li>)}
+			{console.log(todoList)}
+			
 			
 
 
