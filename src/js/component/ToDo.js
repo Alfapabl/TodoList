8334bd4faef58285b00b204fa16e5d9b5144eecf
Pablo Méndez <pablo-mendez09@hotmail.com>
2022-09-12 21:45:39 +0000

    Funcionalidad trabajando falta agregar la parte visual

--- src/js/component/ToDo.js
+++ src/js/component/ToDo.js
@@ -5,15 +5,38 @@ import React, {useState} from 'react';
 const ToDo = () => {
 	const [todoValue, setTodo ] = useState('');
 	const [todoList, setTodoList ] = useState([]);
-	const changeList = () => {
-		{setTodoList([...todoList, todoValue])}
+	
+	const changeList = (e) => {
+		if(e.key == 'Enter'){
+			{setTodoList([...todoList, todoValue])}
+		}
+		
+		
+	}
+	const deleteList = (index) => {
+		let newList = todoList.filter((e, i)=>{
+			if (index !== i){
+				return e;
+			}
+		})
+			setTodoList(newList);
+			console.log(todoList)
+		
 	}
 
+
+
 	return (
-		<div >
+		<div class="align-self-center">
 			<h1>ToDos</h1>
-			<input type="text" placeholder="What needs to be done" onChange={e => setTodo(e.target.value)} onClick={() => changeList()}/>
-			{todoList.map((item)=><li>{item}</li>)}
+			<input type="text" placeholder="What needs to be done" 
+			onChange={e => setTodo(e.target.value)} 
+			onKeyDown={changeList}/> 
+			
+			{todoList.map((item, index)=><li key={index}>{item}
+			<span onClick={()=>deleteList(index)}> x </span>
+			</li>)}
+			<div>{todoList.length} items left</div> 
 			{console.log(todoList)}
 			
 			
